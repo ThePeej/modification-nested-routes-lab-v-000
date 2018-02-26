2e8748afdca1dc37db541f8864b37f88f34ebbb4
PJ Frias <peejfrias@gmail.com>
2018-02-26 00:52:52 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,6 +3,7 @@ module SongsHelper
     if song.author.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "song[author_id]", song.artist_id
+      hidden_field_tag "song[artist_id]", song.artist_id
     end
+  end
 end
