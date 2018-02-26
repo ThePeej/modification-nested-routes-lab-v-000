86cdf6417fbf0e39bf1c6b9235d3c8497ca1388b
PJ Frias <peejfrias@gmail.com>
2018-02-26 00:45:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,9 +50,9 @@ class SongsController < ApplicationController
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
-        (artist_id: params[:artist_id])
-      else
         redirect_to artists_path
+      else
+        @song = artist.songs.find(params[:id])
       end
     else
       @song = Song.find(params[:id])
